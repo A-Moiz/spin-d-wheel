e99c4cd118a4a7e72a42e04f5aa9b0e200bf44aa
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2026-02-26 20:47:20 +0000

    Added pop up for enhanced UI

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 let items = [];
 let generatedItem = "";
+let currentGeneratedIndex = -1;
+
 const appearanceBtn = document.getElementById("appearance-btn");
 const addBtn = document.getElementById("add-btn");
 const clearBtn = document.getElementById("clear-btn");
 const itemsList = document.getElementById("items-list");
 const itemsTitle = document.getElementById("items-title");
-const result = document.getElementById("result");
 const generateBtn = document.getElementById("generate-btn");
+
+const modalOverlay = document.getElementById("modal-overlay");
+const modalItemText = document.getElementById("modal-generated-item");
+const modalRemoveBtn = document.getElementById("modal-remove-btn");
+const modalAgainBtn = document.getElementById("modal-again-btn");
+const modalCloseBtn = document.getElementById("modal-close-btn");
 
 appearanceBtn.addEventListener("click", changeAppearance);
 addBtn.addEventListener("click", addItem);
@@ -54,6 +61,7 @@ function renderItems() {
   items.forEach((item, index) => {
     const li = document.createElement("li");
     li.textContent = item;
+    li.className = "list-item";
 
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
@@ -75,15 +83,35 @@ function clearItems() {
   result.textContent = `Result `;
 }
 
-// Generate random item from the list
+// Generate random item from list
 function generateRandomItem() {
-  if (items.length === 0) {
-    alert("Please add some items to the list first.");
+  if (items.length < 2) {
+    alert("Please add at least 2 items.");
     return;
   }
-  generatedItem = items[Math.floor(Math.random() * items.length)];
-  result.textContent = `Result: ${generatedItem}`;
+
+  currentGeneratedIndex = Math.floor(Math.random() * items.length);
+  generatedItem = items[currentGeneratedIndex];
+
+  modalItemText.textContent = generatedItem;
+  modalOverlay.classList.add("active");
 }
+
+// Button: Remove from list
+modalRemoveBtn.addEventListener("click", () => {
+  items.splice(currentGeneratedIndex, 1);
+  renderItems();
+  modalOverlay.classList.remove("active");
+  result.textContent = "Result";
+});
+
+// Button: Generate again
+modalAgainBtn.addEventListener("click", generateRandomItem);
+
+// Button: Add more items
+modalCloseBtn.addEventListener("click", () => {
+  modalOverlay.classList.remove("active");
+});
 
 // Delete item from the list
 function deleteItem(index) {
