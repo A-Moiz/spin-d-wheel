3764d67cb6ab3ab655e3aca2eda764bb7ff63d1f
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2026-02-26 15:57:22 +0000

    Adding and displaying items in list

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+let items = [];
 const appearanceBtn = document.getElementById("appearance-btn");
+const addBtn = document.getElementById("add-btn");
+const clearBtn = document.getElementById("clear-btn");
+const itemsList = document.getElementById("items-list");
+const itemsTitle = document.getElementById("items-title");
 
 appearanceBtn.addEventListener("click", changeAppearance);
+addBtn.addEventListener("click", addItem);
+clearBtn.addEventListener("click", clearItems);
+itemsTitle.textContent = `Items (${items.length})`;
 
 // Change website appearance
 function changeAppearance() {
@@ -11,4 +19,39 @@ function changeAppearance() {
   } else {
     appearanceBtn.src = "./assets/sun-icon.png";
   }
+}
+
+// Add item to the list
+function addItem(event) {
+  event.preventDefault();
+  const input = document.getElementById("text-input");
+  const item = input.value.trim();
+
+  if (items.includes(item)) {
+    alert("This item already exists in the list.");
+    return;
+  } else {
+    items.push(item);
+    input.value = "";
+    renderItems();
+  }
+}
+
+// Rendering items
+function renderItems() {
+  itemsList.innerHTML = "";
+  itemsTitle.textContent = `Items (${items.length})`;
+
+  // create <li> for each item in items
+  items.forEach((item) => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    itemsList.appendChild(li);
+  });
+}
+
+// Clear all items from the list
+function clearItems() {
+  items = [];
+  renderItems();
 }
